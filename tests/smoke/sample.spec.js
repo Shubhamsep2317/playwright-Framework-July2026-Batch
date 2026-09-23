289633236e61075632cdc7c2e65291765b4acd4e
abcom <shubham.sharma@proscore.ai>
2026-09-23 09:25:48 +0530

    Reapply "Framework First Commit Playwright/JS"
    This reverts commit 76620aecf6cbdac138fc6a28446b694d49c4be60.

---
+++ tests/smoke/sample.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Sample Test",{tags:["sample"]},()=>{
+    
+    test('TC001', async ({ page }) => {
+        
+    });
+
+    test('TC002', async ({ page }) => {
+        
+    });
+
+    test('TC003', async ({ page }) => {
+        
+    });
+
+});
